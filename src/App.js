a9c9e53224adcc4135b06f88f6dd8c4d70acca72
--unset <yowilamezquita380@hotmail.com>
2022-09-29 14:00:05 -0400

    Hooks Personalizados

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ContadorHooks from './components/ContadorHooks'
 import ScroolHooks from './components/ScroolHooks'
 import RelojHooks from './components/RelojHooks'
 import AjaxHooks from './components/AjaxHooks'
+import HooksPersonalizado from './components/HooksPersonalizados'
 
 function App() {
   return (
@@ -71,6 +72,8 @@ function App() {
           <RelojHooks/>
           <hr></hr>
           <AjaxHooks/>
+          <hr></hr>
+          <HooksPersonalizado/>
         </section>
       </header>
     </div>
